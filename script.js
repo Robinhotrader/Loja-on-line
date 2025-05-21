5f0386545c11a56ad8ac98524281f4bbd5d0ae12
Robinho <caminhaspet06@gmail.com>
2025-05-21 14:39:36 -0300

    iptv commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('subscribeButton').addEventListener('click', function() {
+    alert('Obrigado por assinar o IPTV Premium! Redirecionando para o pagamento...');
+    // Aqui você pode redirecionar para a página de pagamento
+     window.location.href = 'https://loja.infinitepay.io/robinhotrader07/utd3931-acesso-mensal-iptv';
+});
